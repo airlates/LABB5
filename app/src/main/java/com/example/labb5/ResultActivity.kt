4e974edb90b9130254b08a9935db7fda1e0b9cd1
airlates <rakov1337@gmail.com>
2025-06-19 18:39:18 +0700

    Создание и работа с Result activ

---
+++ app/src/main/java/com/example/labb5/ResultActivity.kt
@@ -0,0 +1,28 @@
+package com.example.labb5
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var textViewResult: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+
+        textViewResult = findViewById(R.id.textViewResult)
+
+        val totalAmount = intent.getDoubleExtra("TOTAL_AMOUNT", 0.0)
+        textViewResult.text = "Получить = %.2f рублей".format(totalAmount)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
